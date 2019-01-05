250d7884a2c2ac65a00e849882ab2d7d98f4d984
WIN8 <WIN8@192.168.0.12>
2019-01-04 23:02:15 -0400

    Iniciando el proyecto

---
+++ src/main/java/com/app/em/app/biotec/BiotecApplication.java
@@ -0,0 +1,14 @@
+package com.app.em.app.biotec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiotecApplication.class, args);
+	}
+
+}
+
