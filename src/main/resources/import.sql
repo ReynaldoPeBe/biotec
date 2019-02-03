46e8932417880db2a2c36d5c9489ef9a0f596fe4
Jes <Jes@192.168.64.1>
2019-02-02 22:39:51 -0400

    Configurado app.properties para acceso a postgreSQL

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into asistentes (id, nombre, apellido_paterno,fecha_creacion) values (1, 'Eithan Marvin', 'Perez', '2019-01-18');
-insert into asistentes (id, nombre, apellido_paterno,fecha_creacion) values (2, 'Matias Kaleb', 'Tarqui', '2019-01-18');
-insert into asistentes (id, nombre, apellido_paterno,fecha_creacion) values (3, 'Andrea Adela', 'Velasquez', '2019-01-18');
-insert into asistentes (id, nombre, apellido_paterno,fecha_creacion) values (4, 'Jesus Reynaldo', 'Perez', '2019-01-18');
